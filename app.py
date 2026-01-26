3af411431bf2cae02aff954baa6cb19df694666e
Varadrajan2021 <Varadrajanjayant.Kunsavalikar@genzeon.com>
2026-01-26 23:04:30 +0530

    tried differnet model - did a litmus test - yet benchmarking is not done - going with MODEL_ID = os.getenv(MODEL_ID, Gustking/wav2vec2-large-xlsr-deepfake-audio-classification)

--- app.py
+++ app.py
@@ -22,9 +22,12 @@ DEFAULT_TOPK = int(os.getenv("ROBUST_TOPK", "3"))
 HOP_SECONDS = float(os.getenv("ROBUST_HOP_SECONDS", "2.0"))
 
 # New model (spoof vs bonafide)
-MODEL_ID = os.getenv("MODEL_ID", "WWWxp/wav2vec2_spoof_dection1")
+# MODEL_ID = os.getenv("MODEL_ID", "WWWxp/wav2vec2_spoof_dection1") # can be improved - working
+# MODEL_ID = os.getenv("MODEL_ID", "HyperMoon/wav2vec2-base-960h-finetuned-deepfake") # straight no
 
-
+# MODEL_ID = os.getenv("MODEL_ID", "Gustking/wav2vec2-large-xlsr-deepfake-audio-classification") # best yet
+# MODEL_ID = os.getenv("MODEL_ID", "DavidCombei/wavLM-base-Deepfake_V2") # try in benchmarking not reliable though- can't say- benchmark
+# MODEL_ID = os.getenv("MODEL_ID", "microsoft/wavlm-base-plus-sv") # try another time - getting some error  from layers
 # -------------------- Model --------------------
 def get_device() -> torch.device:
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
